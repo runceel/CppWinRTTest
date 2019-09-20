50f9269c511dfe0b5a6d4f164e80978d5accff78
Kazuki Ota <kaota@microsoft.com>
2019-09-20 15:20:31 +0900

    Step 4

---
+++ CSharpUWPApp/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using MyComponents;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace CSharpUWPApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private Person Person { get; } = new Person { Name = "Tanaka" };
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
